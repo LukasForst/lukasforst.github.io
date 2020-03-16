a39b3eed0ff9572eb19f3532c312a89572150a04
Lukas Forst <lukas@forst.pw>
2020-03-16 21:04:06 +0100

    redirect page

--- js/BandProvider.js
+++
@@ -1,9 +0,0 @@
-export default class BandProvider {
-    constructor(apiData){
-        this._apiData = apiData;
-    }
-
-    displayDataForBand(bandName){
-        $('#name-band-section-header').text(bandName);
-    }
-}
--- js/api/Concert.js
+++
@@ -1,9 +0,0 @@
-export default class Concert {
-    constructor(id, performingBand, date, place, playlist) {
-        this.performingBand = performingBand;
-        this.playlist = playlist;
-        this.date = date;
-        this.place = place;
-        this.id = id;
-    }
-}
